e28827fbc98ea8852a6a11cefbd606453bd33514
Bghd34 <boughadiyacine@yahoo.fr>
2021-09-07 10:21:14 +0200

    add single.js

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from "react";
+import firebase from "firebase/compat";
+import data from './data.json';
+
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {data};
+  }
+
+  componentWillUnmount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBeRdhTM681mPi5zu-3I2gF_3VnXgcaV8s",
+      authDomain: "crm-linkedin-6efa1.firebaseapp.com",
+      projectId: "crm-linkedin-6efa1",
+      storageBucket: "crm-linkedin-6efa1.appspot.com",
+      messagingSenderId: "81571918137",
+      appId: "1:81571918137:web:056230a397b19d997e38e8"
+    })
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
+  }
+
+
+}
+
+export default App;
