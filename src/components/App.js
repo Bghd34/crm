ceb1d06c36038025371af4829c69066242f6eb68
Bghd34 <boughadiyacine@yahoo.fr>
2021-09-09 10:27:48 +0200

    add function updateData

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {Component} from "react";
-import firebase from "firebase/compat";
+import firebase from "../firebase";
 import data from '../data.json';
 import Grid from "./Grid";
 import Form from "./Form";
@@ -9,11 +9,32 @@ class App extends Component{
 
   constructor(props) {
     super(props);
-    this.state = {data};
+    this.state = {
+      contacts: []
+    };
+  }
+  updateData() {
+    const db = firebase.firestore;
+    const settings = {timestampsInSnapshots: true};
+    db.settings(settings);
+    db.collection('contacts').get()
+        .then(snapshot => {
+          let contacts = [];
+          snapshot.forEach(doc =>{
+            let contact = Object.assign({id: doc.id}, doc.data());
+            contacts.push(contact);
+          });
+          this.setState({
+              contacts: contacts
+          });
+        })
+        .catch(err => {
+            console.log('Error!', err);
+        })
   }
 
   componentWillUnmount() {
-
+      this.updateData();
   }
 
   render() {
@@ -28,7 +49,7 @@ class App extends Component{
           </div>
           <div>
             <Form />
-            <Grid items = {this.state.data}/>
+            <Grid items = {this.state.contacts}/>
           </div>
         </div>
     );
